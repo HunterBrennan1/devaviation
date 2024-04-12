2a9aa73c89c3e9d39de2b4705603cefc06c7981e
Hunter <hunterbrennan15@gmail.com>
2024-04-12 07:06:27 -0400

    changed button routing in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -79,7 +79,7 @@ function MobileNavigation() {
             as="div"
             className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
           >
-            <MobileNavLink href="#features">Features</MobileNavLink>
+            <MobileNavLink href="#features">Contact</MobileNavLink>
             <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
             <MobileNavLink href="#pricing">Pricing</MobileNavLink>
             <hr className="m-2 border-slate-300/40" />
@@ -101,16 +101,16 @@ export function Header() {
               <Logo className="h-10 w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
-              <NavLink href="#features">Features</NavLink>
+              <NavLink href="#features">Contact us!</NavLink>
               <NavLink href="#testimonials">Testimonials</NavLink>
               <NavLink href="#pricing">Pricing</NavLink>
             </div>
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
             <div className="hidden md:block">
-              <NavLink href="/login">Sign in</NavLink>
+              <NavLink href="/https://www.instagram.com/devaneyaviation/?igsh=OWJ4bjg0N2F0eWdu&utm_source=qr" target="_blank">Instagram</NavLink>
             </div>
-            <Button href="/register" color="blue">
+            <Button href="#pricing" color="blue">
               <span>
                 Get started <span className="hidden lg:inline">today</span>
               </span>
