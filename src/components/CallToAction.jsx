1364fe8f47552acfdb73bf6ee844abe964d35dab
Hunter <hunterbrennan15@gmail.com>
2024-04-12 05:35:05 -0400

    added pricing link button to rental section

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -8,25 +8,25 @@ export function CallToAction() {
   return (
     <section
       id="get-started-today"
-      className="relative overflow-hidden bg-blue-600 py-32"
+      className="relative overflow-hidden bg-white py-16"
     >
-      <Image
+      {/* <Image
         className="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
         src={backgroundImage}
         alt=""
         width={2347}
         height={1244}
         unoptimized
-      />
+      /> */}
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
-          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
+          <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
             Follow us on instagram!
           </h2>
-          <p className="mt-4 text-lg tracking-tight text-white">
+          <p className="mt-4 text-lg tracking-tight text-slate-600">
             Keep up to date with all the latest deals and offers! Follow us today to get a deeper look into our goals and plans for the future.
           </p>
-          <Button href="https://www.instagram.com/devaneyaviation/?igsh=OWJ4bjg0N2F0eWdu&utm_source=qr" target="_blank" color="white" className="mt-10">
+          <Button href="https://www.instagram.com/devaneyaviation/?igsh=OWJ4bjg0N2F0eWdu&utm_source=qr" target="_blank" color="blue" className="mt-10">
             Instagram: @Devaneyaviation
           </Button>
         </div>
--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -61,6 +61,9 @@ export function SecondaryFeatures() {
         </div>
         </div>
       </Container>
+      <div className="w-full text-center mt-10">
+      <a class="group inline-flex items-center justify-center rounded-full py-2 px-8 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600 mt-10" color="blue" variant="solid" href="#pricing">See Pricing!</a>
+      </div>
     </section>
   )
 }
